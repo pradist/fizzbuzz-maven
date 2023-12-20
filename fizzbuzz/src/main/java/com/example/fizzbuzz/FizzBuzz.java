607926c01ee35fddd52a7e4fdf86942ce2a9dbb4
Pradist Keawborisut <11456712@krungsri.com>
2023-12-20 18:28:07 +0700

    refactor: return number step 2

--- fizzbuzz/src/main/java/com/example/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/example/fizzbuzz/FizzBuzz.java
@@ -2,12 +2,6 @@ package com.example.fizzbuzz;
 
 public class FizzBuzz {
     public String Generate(int number) {
-        if (number == 2) {
-            return String.valueOf(number);
-        }
-        if (number == 4) {
-            return String.valueOf(number);
-        }
         return String.valueOf(number);
     }
 }
