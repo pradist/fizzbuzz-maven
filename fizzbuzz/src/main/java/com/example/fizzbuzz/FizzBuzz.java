f48180e8643d56dfc169997aae14f4b447a5ad39
Pradist Keawborisut <11456712@krungsri.com>
2023-12-20 17:46:33 +0700

    feat: add test case fizzbuzz_whenInputIs1_shouldReturn1

---
+++ fizzbuzz/src/main/java/com/example/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.example.fizzbuzz;
+
+public class FizzBuzz {
+    public String Generate(int number) {
+        return "1";
+    }
+}
--- fizzbuzz/src/test/java/com/example/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/example/fizzbuzz/FizzBuzzTest.java
@@ -5,5 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    @Test
+    public void fizzbuzz_whenInputIs1_shouldReturn1() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String expected = "1";
+        String actual = fizzbuzz.Generate(1);
 
+        assertEquals(expected, actual);
+    }
 }
