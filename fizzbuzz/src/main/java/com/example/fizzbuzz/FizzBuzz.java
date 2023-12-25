418c85d0cd07c1a75749bc969c7cb852188749db
Pradist Keawborisut <pradist.ke@gmail.com>
2023-12-25 20:49:55 +0700

    refactor: reformat code

--- fizzbuzz/src/main/java/com/example/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/example/fizzbuzz/FizzBuzz.java
@@ -1,16 +1,16 @@
 package com.example.fizzbuzz;
 
 public class FizzBuzz {
-    public String Generate(int number) {
-        if (number == 15) {
-            return "FizzBuzz";
-        }
-        if (number % 3 == 0) {
-            return "Fizz";
-        }
-        if (number % 5 ==0) {
-            return "Buzz";
-        }
-        return String.valueOf(number);
+  public String Generate(int number) {
+    if (number == 15) {
+      return "FizzBuzz";
     }
+    if (number % 3 == 0) {
+      return "Fizz";
+    }
+    if (number % 5 == 0) {
+      return "Buzz";
+    }
+    return String.valueOf(number);
+  }
 }
--- fizzbuzz/src/test/java/com/example/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/example/fizzbuzz/FizzBuzzTest.java
@@ -5,93 +5,93 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-    @Test
-    public void whenInputIs1_shouldReturn1() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "1";
-        String actual = fizzbuzz.Generate(1);
+  @Test
+  public void whenInputIs1_shouldReturn1() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "1";
+    String actual = fizzbuzz.Generate(1);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs2_shouldReturn2() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "2";
-        String actual = fizzbuzz.Generate(2);
+  @Test
+  public void whenInputIs2_shouldReturn2() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "2";
+    String actual = fizzbuzz.Generate(2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs4_shouldReturn4() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "4";
-        String actual = fizzbuzz.Generate(4);
+  @Test
+  public void whenInputIs4_shouldReturn4() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "4";
+    String actual = fizzbuzz.Generate(4);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs3_shouldReturnFizz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "Fizz";
-        String actual = fizzbuzz.Generate(3);
+  @Test
+  public void whenInputIs3_shouldReturnFizz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "Fizz";
+    String actual = fizzbuzz.Generate(3);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs6_shouldReturnFizz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "Fizz";
-        String actual = fizzbuzz.Generate(6);
+  @Test
+  public void whenInputIs6_shouldReturnFizz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "Fizz";
+    String actual = fizzbuzz.Generate(6);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs9_shouldReturnFizz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "Fizz";
-        String actual = fizzbuzz.Generate(9);
+  @Test
+  public void whenInputIs9_shouldReturnFizz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "Fizz";
+    String actual = fizzbuzz.Generate(9);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs5_shouldReturnBuzz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "Buzz";
-        String actual = fizzbuzz.Generate(5);
+  @Test
+  public void whenInputIs5_shouldReturnBuzz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "Buzz";
+    String actual = fizzbuzz.Generate(5);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs10_shouldReturnBuzz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "Buzz";
-        String actual = fizzbuzz.Generate(10);
+  @Test
+  public void whenInputIs10_shouldReturnBuzz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "Buzz";
+    String actual = fizzbuzz.Generate(10);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs20_shouldReturnBuzz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "Buzz";
-        String actual = fizzbuzz.Generate(20);
+  @Test
+  public void whenInputIs20_shouldReturnBuzz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "Buzz";
+    String actual = fizzbuzz.Generate(20);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void whenInputIs15_shouldReturnFizzBuzz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        String expected = "FizzBuzz";
-        String actual = fizzbuzz.Generate(15);
+  @Test
+  public void whenInputIs15_shouldReturnFizzBuzz() {
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    String expected = "FizzBuzz";
+    String actual = fizzbuzz.Generate(15);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 }
